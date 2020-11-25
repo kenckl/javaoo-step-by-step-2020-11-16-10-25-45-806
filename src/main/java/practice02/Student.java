f3def9d7f8541a62b6c707557b5d5208289c0060
kenckl <klcheung8355@gmail.com>
2020-11-25 14:46:08 +0800

    [Ken] finish practice 02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,23 @@
 package practice02;
 
-public class Student {
+import practice01.Person;
+
+public class Student extends Person {
+    private String name;
+    private int age;
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+
+        return "I am a Student. I am at Class 2.";
+    }
 }
