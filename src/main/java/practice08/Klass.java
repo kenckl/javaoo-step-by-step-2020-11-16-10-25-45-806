1dca8ea0b78364974892b959f8769e99d8d3b365
kenckl <klcheung8355@gmail.com>
2020-11-25 20:54:26 +0800

    [Ken] finished practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,38 @@
 package practice08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students = new ArrayList<Student>();
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.contains(leader))
+            this.leader = leader;
+        else
+            System.out.print("It is not one of us.\n");
+    }
+
+    public void appendMember(Student s){
+        students.add(s);
+    }
 }
+
