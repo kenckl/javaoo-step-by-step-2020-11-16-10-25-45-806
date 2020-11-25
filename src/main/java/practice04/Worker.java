61e8afb7fea4ffcdb60925e763988f23f9a5ef34
kenckl <klcheung8355@gmail.com>
2020-11-25 15:19:20 +0800

    [Ken]finish practice 04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,15 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a Worker. I have a job.";
+    }
 }
