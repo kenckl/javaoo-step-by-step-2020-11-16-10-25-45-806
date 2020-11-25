ec948bed36b1058fff636abc9018bcffc58298e5
kenckl <klcheung8355@gmail.com>
2020-11-25 16:41:20 +0800

    [Ken] finished practice06

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,18 @@
 package practice06;
 
 public class Klass {
+    private int number;
+    private String displayName;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
