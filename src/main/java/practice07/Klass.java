9228b96ac66eed38cae4bd92f6c4f833a9155702
kenckl <klcheung8355@gmail.com>
2020-11-25 18:06:28 +0800

    [Ken]finish practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,26 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
 }
