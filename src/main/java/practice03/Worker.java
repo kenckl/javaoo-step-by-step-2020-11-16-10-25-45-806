861ef8a58d2a225efbe6274423159835c7f100fc
kenckl <klcheung8355@gmail.com>
2020-11-25 14:53:15 +0800

    [Ken] finish practice 03

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,16 @@
 package practice03;
 
-public class Worker {
+import practice01.Person;
+
+public class Worker extends Person {
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+        return "I am a Worker. I have a job.";
+    }
 }
